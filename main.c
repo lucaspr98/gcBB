4fd80766df83f0137d135bb003dabfe91933a8c6
lucaspr98 <lucaspr98@gmail.com>
2021-05-01 09:10:46 -0400

    small fix on bwsd

--- main.c
+++ main.c
@@ -15,15 +15,8 @@ double bwsd_expectation(int *t, int s, int n);
 
 double bwsd_shannon_entropy(int *t, int s, int n);
 
-// void boss_example(); // do we want to compute boss to a single genome? If we want we need a few ifdef
-
-void colored_boss_example();
-
 int main(int argc, char *argv[]){
     int i;
-
-    // colored_boss_example();
-    // exit(-1);
 
     /******** Check arguments and compute external needed files ********/
 
@@ -37,7 +30,7 @@ int main(int argc, char *argv[]){
     char *file2 = argv[2];
     int k = atoi(argv[3]);
     
-    // // Computes BWT, LCP and DA from both files
+    // Computes SA, BWT, LCP and DA from both files
     compute_files(file1, file2, k);
 
     FILE *mergeBWT = fopen("merge.bwt", "r");
@@ -100,77 +93,13 @@ void compute_files(char *file1, char *file2, int k){
     char eGap2[128];
     char eGapMerge[128];
 
-    sprintf(eGap1, "egap/eGap --trlcp %d --lbytes 4 --da %s", k, file1);
-    sprintf(eGap2, "egap/eGap --trlcp %d --lbytes 4 --da %s", k, file2);
-    sprintf(eGapMerge, "egap/eGap --bwt --trlcp %d --lbytes 4 --da -o merge %s.bwt %s.bwt", k, file1, file2);
+    sprintf(eGap1, "egap/eGap -m 4096 --trlcp %d --rev --lbytes 4 --da %s", k,  file1);
+    sprintf(eGap2, "egap/eGap -m 4096 --trlcp %d --rev --lbytes 4 --da %s", k, file2);
+    sprintf(eGapMerge, "egap/eGap -m 4096 --trlcp %d --rev --bwt --lbytes 4 --da -o merge %s.bwt %s.bwt", k, file1, file2);
 
     system(eGap1);
     system(eGap2);
     system(eGapMerge);
-}
-
-// void boss_example() {
-//     int n0, n1, k;
-//     n0 = 14;
-//     n1 = 7;
-//     k = 3;
-
-//     char BWT_0[n0+1];
-//     strcpy(BWT_0, "TTC$CCTATAAA$C");
-//     int LCP_0[] = {-1,3,0,2,1,3,0,2,3,1,0,3,1,3};
-
-//     char BWT_1[n1+1]; 
-//     strcpy(BWT_1, "GCTGA$C");
-//     int LCP_1[] = {-1,0,0,1,0,1,0};
-    
-//     /*** Construct BOSS representation ***/
-
-//     int *last_0 = (int*)malloc((n0)*sizeof(int*));
-//     int *Wm_0 = (int*)malloc((n0)*sizeof(int*));
-
-//     int *last_1 = (int*)malloc((n1)*sizeof(int*));
-//     int *Wm_1 = (int*)malloc((n1)*sizeof(int*));
-
-//     // create and initialize C map
-//     int C_0[255];
-//     memset(C_0, 0, sizeof(int)*255);
-//     int C_1[255];
-//     memset(C_1, 0, sizeof(int)*255);
-
-//     // create and initizalize W string
-//     char *W_0 = (char*)malloc((n0)*sizeof(char*));
-//     char *W_1 = (char*)malloc((n1)*sizeof(char*));
-
-//     // BOSS construction
-//     boss_construction(LCP_0, NULL, BWT_0, C_0, last_0, W_0, Wm_0, NULL, n0, k, 1);
-//     boss_construction(LCP_1, NULL, BWT_1, C_1, last_1, W_1, Wm_1, NULL, n1, k, 1);
-// }
-
-void colored_boss_example() {
-    int n, k;
-    n = 23;
-    k = 3;
-
-    char BWT[n+1];
-    strcpy(BWT, "TTGC$CCCTTATGAA$AA$CCTT");
-    int LCP[] = {-1,3,3,0,2,1,1,3,0,2,2,3,1,4,0,1,0,3,1,3,3,4,6};
-    int DA[] = {0,0,1,0,0,1,0,0,0,1,0,0,1,0,1,1,0,0,0,1,0,1,0};
-    
-    /*** Construct BOSS representation ***/
-
-    int *last = (int*)malloc((n)*sizeof(int*));
-    int *Wm = (int*)malloc((n)*sizeof(int*));
-    int *colors = (int*)malloc((n)*sizeof(int*));
-
-    // create and initialize C map
-    int C[255];
-    memset(C, 0, sizeof(int)*255);
-
-    // create and initizalize W string
-    char *W = (char*)malloc((n)*sizeof(char*));
-
-    // BOSS construction
-    boss_construction(LCP, DA, BWT, C, last, W, Wm, colors, n, k, 2);
 }
 
 void Wi_sort(char *Wi, int *Wm, int *colors, int start, int end){
@@ -353,9 +282,9 @@ void boss_construction(int *LCP, int *DA, char *BWT, int *C, int *last, char *W,
     printf("\n");
 
     printf("BOSS:\nlast\tW\tW-\tcolor\n");
-    for(j = 0; j < i; j++){
+
+    for(j = 0; j < i; j++)
         printf("%d\t\t%c\t%d\t%d\n", last[j], W[j], Wm[j], colors[j]);
-    }
 };
 
 double bwsd_expectation(int *t, int s, int n){
@@ -391,7 +320,7 @@ double bwsd_shannon_entropy(int *t, int s, int n){
 void bwsd(int *DA, int n, double *expectation, double *entropy){
     int i;
 
-    int *run_length = (int*)malloc((n+6)*sizeof(int));
+    int *run_length = (int*)malloc((n+10)*sizeof(int));
     int current = 0;
     run_length[0] = 0;
     run_length[1] = 0;
@@ -413,8 +342,8 @@ void bwsd(int *DA, int n, double *expectation, double *entropy){
     }
     pos++; //size of run_lentgh
 
-    int t[n];
-    memset(t, 0, (n)*sizeof(int));
+    int *t = (int*)malloc(n*sizeof(int));
+    memset(t, 0, (n+10)*sizeof(int));
     for(i = 0; i < pos; i+=2){
         t[run_length[i+1]]++;
     }
@@ -425,4 +354,3 @@ void bwsd(int *DA, int n, double *expectation, double *entropy){
     printf("BWSD:\n");
     printf("Expectation: %.5lf\nShannons Entropy: %.5lf\n", *expectation, *entropy);
 }
-
